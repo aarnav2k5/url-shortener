bdebcef067cc1d9b288b7c7f6d093ae3c0560ca0
aarnav2k5 <jaiswalaarnav@gmail.com>
2025-09-07 00:27:26 +0530

    add backend

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(cors());
 
 // Connect to MongoDB
-mongoose.connect("mongodb://127.0.0.1:27017/urlShortener", {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
